36bdaa7f0703f4b977da7f2172a4790ff1b12d8b
Suin Kim <contact@suinkim.me>
2025-10-25 19:01:03 +0900

    chore: Radix UI 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>{children}</Theme>
+        {children}
       </body>
     </html>
   );
